a49b1ad3b7780cb0f26ea7cd51ee84f199752062
Uday Chauhan <uday.chauhan0408@gmail.com>
2025-09-18 18:55:55 +0530

    Added somethins in 18_09_2025

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173","http://localhost:5173"],
     credentials: true,
   })
 );
